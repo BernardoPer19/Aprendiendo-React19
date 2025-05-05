8cfc9b43a582cbbf450492845e2e93e3508f5454
jhoaking <wawxper08@gmail.com>
2025-05-05 11:32:58 -1100

    usando el hook 'use' para peticiones HTTP en React, inicializando el proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import Planets from './pages/Planets';
+import { Suspense } from "react";
+import { getPlanets } from "./actions/get-planets.actions";
+import Planets from "./pages/Planets";
 
 function App() {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Planetas del Sistema Solar</h1>
 
-      <Planets />
+      <Suspense fallback={<h1>Cargando planetas...</h1>}>
+        <Planets getPlanets={getPlanets()} />
+      </Suspense>
     </div>
   );
 }
