e91f2e11db632bd7da2cf07709a8448d1e39442d
jhoaking <wawxper08@gmail.com>
2025-05-05 12:21:28 -1100

    añadiendo ErrorBoundary en React para los Errores en Suspense y el API de 'use'

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Suspense } from "react";
 import { getPlanets } from "./actions/get-planets.actions";
 import Planets from "./pages/Planets";
+import { ErrorBoundary } from "./shared/ErrorBoundary";
 
 function App() {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Planetas del Sistema Solar</h1>
 
-      <Suspense fallback={<h1>Cargando planetas...</h1>}>
-        <Planets getPlanets={getPlanets()} />
-      </Suspense>
+      <ErrorBoundary fallback={<h1>Error al cargar los planetas</h1>}>
+        <Suspense fallback={<h1>Cargando planetas...</h1>}>
+          <Planets getPlanets={getPlanets()} />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
--- src/api/planetsApi.ts
+++ src/api/planetsApi.ts
@@ -4,12 +4,11 @@ export const planetsApi = axios.create({
   baseURL: 'http://localhost:3100/planets',
 });
 
-//! Interceptor para simular una espera de 2 segundos
 planetsApi.interceptors.request.use((config) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(config);
-      // reject(new Error('Error de prueba desde interceptor'));
+      reject(new Error('Error de prueba desde interceptor'));
     }, 2000);
   });
 });
